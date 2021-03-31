133fe840eace6db7f790f08d935dbe0ee3dff6f3
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-03-31 17:31:10 -0300

    Adicionada classe ActorMovie

--- Persistencia/Entidades/Actor.cs
+++ Persistencia/Entidades/Actor.cs
@@ -2,9 +2,10 @@
 using System.Text;
 
 namespace Persistencia.Entidades
+
 public class Actor
 {
-    public int ActorId;
-    public String Name;
-    public DateTime DateBirth;
+    public int ActorId { get; set; }
+    public String Name { get; set; }
+    public DateTime DateBirth { get; set; }
 }
---
+++ Persistencia/Entidades/ActorMovie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace Persistencia.Entidades
+
+public class ActorMovie
+{
+    public int ActorMovieId { get; set; }
+    public String Character { get; set; }
+    public virtual Movie Movie { get; set; }
+    public virtual Actor Actor { get; set; }
+}
