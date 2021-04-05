3908de5273ed0974e3d04f99ad74ab2e8ccb544e
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-04-05 20:44:31 -0300

    Insercao dos métodos virtual em ActorMovie e adicao da primeira consulta.

--- Persistencia/Entidades/ActorMovie.cs
+++ Persistencia/Entidades/ActorMovie.cs
@@ -8,6 +8,8 @@ namespace Persistencia.Entidades
         public int ActorMovieId { get; set; }
         public String Character { get; set; }
         public int MovieId { get; set; }
-        public int ActorId { get; set; } 
+        public virtual Movie Movie { get; set; }
+        public int ActorId { get; set; }
+        public virtual Actor Actor { get; set; }
     }
 }
