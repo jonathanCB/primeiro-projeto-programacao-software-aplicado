5545586828a8fe7f8941d3c7a80734e6788b0f03
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-04-07 11:49:09 -0300

    Consulta 4 feita.

--- Persistencia/Program.cs
+++ Persistencia/Program.cs
@@ -16,7 +16,7 @@ namespace Persistencia
             MovieContext _context = new MovieContext();
 
             #region Consulta 1
-            Console.WriteLine("1 - Listar o nome de todos personagens desempenhados por um determinado " +
+            Console.WriteLine(" 1 - Listar o nome de todos personagens desempenhados por um determinado " +
                 "ator (Carrie Fischer), incluindo a informação de qual o filme.");
 
             var c1 = from c in _context.Characters
@@ -35,7 +35,7 @@ namespace Persistencia
             #endregion
 
             #region Consulta 2
-            Console.WriteLine("\n2 - Mostrar o nome de todos atores que desempenharam um determinado " +
+            Console.WriteLine("\n 2 - Mostrar o nome de todos atores que desempenharam um determinado " +
                 "personagem (por exemplo, quais os atores que já atuaram como '007'?)");
 
             var a1 = from a in _context.Characters
@@ -49,12 +49,12 @@ namespace Persistencia
 
             foreach (var ator in a1)
             {
-                Console.WriteLine("\n {0} estrelando {1} como {2}\n", ator.Title, ator.Name, ator.Character);
+                Console.WriteLine("\n {0} estrelando {1} como {2}.\n", ator.Title, ator.Name, ator.Character);
             }
             #endregion
 
             #region Consulta 3
-            Console.WriteLine("3 - Informar qual o ator desempenhou mais vezes um determinado " +
+            Console.WriteLine(" 3 - Informar qual o ator desempenhou mais vezes um determinado " +
                 "personagem (por exemplo: qual o ator que realizou mais filmes como o 'agente 007') ");
 
             var a2 = (from a in _context.Characters
@@ -74,7 +74,7 @@ namespace Persistencia
                         "QtdFilmes: {1}", a2.Nome, a2.NroPersonagens);
 
             //------------------------------------------------------------------------------------------
-            
+
             /*Segunda forma: Tirar o o método .OrderByDescending e o .FirstOrDefault()
             da consulta e trabalhar com variáveis.*/
 
@@ -84,7 +84,7 @@ namespace Persistencia
             "QtdFilmes: {1}", nomeAtor, qtdVezesComoPersonagem);*/
 
             //------------------------------------------------------------------------------------------
-            
+
             //Terceira forma:
             /*int maisVezes = 0;
             foreach (var ator in a2.OrderByDescending(a => a.NroPersonagens))
@@ -98,7 +98,7 @@ namespace Persistencia
             };*/
 
             //------------------------------------------------------------------------------------------
-            
+
             //Quarta forma: Mostrar todos os atores que desempenharam o personagem escolhido na ordem decrescente:
             /*foreach (var ator in a2.OrderByDescending(a => a.NroPersonagens))
             {
@@ -108,8 +108,59 @@ namespace Persistencia
             #endregion
 
             #region Consulta 4
-            /*var a3 = from a in _context.Actors
-                where a.DateBirth*/
+            Console.WriteLine("\n 4 - Mostrar o nome e a data de nascimento do ator mais idoso e o mais novo ");
+
+            //Ator mais idoso
+            var dataMenor = (from a in _context.Actors
+                             select a.DateBirth).Min();
+
+            String atorMaisVelho = (from a in _context.Actors
+                                 where a.DateBirth == dataMenor
+                                 select a.Name).FirstOrDefault();
+
+            Console.WriteLine("\n Nome do ator mais velho: {0}\n Data de nascimento: {1}",
+                atorMaisVelho, dataMenor);
+
+            //Ator mais novo
+            var dataMaior = (from a in _context.Actors
+                             select a.DateBirth).Max();
+
+            String atorMaisNovo = (from a in _context.Actors
+                                where a.DateBirth == dataMaior
+                                select a.Name).FirstOrDefault();
+
+            Console.WriteLine("\n Nome do ator mais novo: {0}\n Data de nascimento: {1}",
+                atorMaisNovo, dataMaior);
+
+            /*
+            //Ator mais velho
+            var a3 = (from a in _context.Actors
+                     group a by a.Name into grupo
+                     select new
+                     {
+                         Nome = grupo.Key,
+                         DataNascimentoAtorMaisVelho = grupo.Min(t => t.DateBirth)
+                     })
+                     .OrderByDescending(a => a.DataNascimentoAtorMaisVelho)
+                     .FirstOrDefault();
+
+            Console.WriteLine("\n Nome do ator mais novo: {0}\n Data de nascimento: {1}", 
+                a3.Nome, a3.DataNascimentoAtorMaisVelho);
+
+            //Ator mais novo
+            var a4 = (from a in _context.Actors
+                      group a by a.Name into grupo
+                      select new
+                      {
+                          Nome = grupo.Key,
+                          DataNascimentoAtorMaisNovo = grupo.Max(t => t.DateBirth)
+                      })
+                     .OrderByDescending(a => a.DataNascimentoAtorMaisNovo)
+                     .FirstOrDefault();
+
+            Console.WriteLine("\n Nome do ator mais novo: {0}\n Data de nascimento: {1}",
+                a4.Nome, a4.DataNascimentoAtorMaisNovo);
+            */
             #endregion
         }
     }
